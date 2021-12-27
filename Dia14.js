84cdd42068e7ec43027ba62988e73af7e9351fa6
Damián Hernández Huchín <huchin.hdamian@gmail.com>
2021-12-26 18:08:35 -0600

    Día 14

---
+++ Dia14.js
@@ -0,0 +1,14 @@
+export default function missingReindeer(ids) {
+  let esperaValor = 0;
+
+  for (let i = 1; i <= ids.length; i++) esperaValor += i;
+
+  return esperaValor - ids.reduce((a, b) => a + b);
+}
+
+console.log(missingReindeer([0, 2, 3])); // -> 1
+console.log(missingReindeer([5, 6, 1, 2, 3, 7, 0])); // -> 4
+console.log(missingReindeer([0, 1])); // -> 2 (¡es el último el que falta!)
+console.log(missingReindeer([3, 0, 1])); // -> 2
+console.log(missingReindeer([9, 2, 3, 5, 6, 4, 7, 0, 1])); // -> 8
+console.log(missingReindeer([0])); // -> 1 (¡es el último el que falta!)
